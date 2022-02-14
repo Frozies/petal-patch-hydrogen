57900b2c046a806f29806c60488f5028778a05e3
Frozies <52686922+Frozies@users.noreply.github.com>
2022-02-14 09:51:47 -0500

    style: adjusted fonts

--- src/components/Welcome.client.tsx
+++ src/components/Welcome.client.tsx
@@ -2,7 +2,7 @@ export default function Welcome() {
   return (
     <div className="text-gray-900 pt-16 rounded-[40px] my-8 px-4 xl:px-12 -mx-4 xl:-mx-12">
       <div className="text-center mb-16">
-        <h1 className="font-sansSerif font-semibold mb-4 text-shadow-xl text-white text-5xl leading-tight">
+        <h1 className="font-sansSerif font-semibold mb-4 text-shadow-xl text-white xs:text-3xl md:text-4xl lg:text-5xl leading-tight">
           This valentine&#39;s Day, get your sweetheart just what they&#39;ve
           wanted (at just the right price).
         </h1>
